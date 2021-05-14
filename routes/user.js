e2fad2de36c6ebd3e6a2622e5cd5cac0514ca2b7
Erik Töger <erik.toger@gmail.com>
2021-05-14 10:16:12 +0200

    Add password encryption

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET;
 const User = require("../userSchema");
+const bcrypt = require("bcrypt");
 
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ name: req.body.name });
-    if (user && user.password === req.body.password) {
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid Credentials" });
+    }
+
+    const result = bcrypt.compareSync(req.body.password, user.password);
+
+    if (result) {
       const token = jwt.sign({ user: req.body.name }, jwtSecret, {
         algorithm: "HS256",
       });
@@ -26,13 +34,16 @@ router.post("/create", async (req, res) => {
     if (user) {
       return res.status(400).json({ error: "Username not available" });
     }
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User({
       name: req.body.name,
       favorites: [],
-      password: req.body.password,
+      password: hash,
     });
-
     await newUser.save();
+
     const token = jwt.sign({ user: req.body.name }, jwtSecret, {
       algorithm: "HS256",
     });
